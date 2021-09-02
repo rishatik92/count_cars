6a4ef3ac1bf1818db3220a0a8c62dafcdac8c933
rishatik92 <Rishatik92@gmail.com>
2021-09-02 18:48:42 +0300

    fix

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ frames_computed = 0
 while video_capture.isOpened():
 
     success, frame = video_capture.read()
-
+    print(type(frame))
     img = cv2.imdecode(frame, cv2.IMREAD_UNCHANGED)
     bbox, label, conf = cv.detect_common_objects(img)
     output_image = draw_bbox(img, bbox, label, conf)

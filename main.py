5bc033d87b49d1323eb36b5bbbd10c799b62e821
rishatik92 <Rishatik92@gmail.com>
2021-09-04 13:36:13 +0300

    fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 # Specify device
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
-import cv2
+import cv2, threading
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
 import telegram
@@ -21,12 +21,24 @@ def send_message(msg):
 
 def send_image(image):
     bot.send_photo(config['chat_id'], image)
+# Define the thread that will continuously pull frames from the camera
+
+class CameraBufferCleanerThread(threading.Thread):
+    def __init__(self, camera, name='camera-buffer-cleaner-thread'):
+        self.camera = camera
+        self.last_frame = None
+        super(CameraBufferCleanerThread, self).__init__(name=name)
+        self.start()
+
+    def run(self):
+        while True:
+            ret, self.last_frame = self.camera.read()
 
 
 VIDEO_SOURCE = config['video_source']
 # Загружаем видеофайл, для которого хотим запустить распознавание.
 video_capture = cv2.VideoCapture(VIDEO_SOURCE)
-
+cam_cleaner = CameraBufferCleanerThread(video_capture)
 # Проходимся в цикле по каждому кадру.
 max_i = 300
 i = 0
@@ -35,8 +47,7 @@ assert every_print < max_i
 frames_computed = 0
 last_string_num = 0
 while video_capture.isOpened():
-
-    success, frame = video_capture.read()
+    success, frame = cam_cleaner.last_frame
 
     i +=1
     if not success:
@@ -55,12 +66,12 @@ while video_capture.isOpened():
     set_label = set(label)
     summary_vehicle = 0
     for type_vehicle in set_label:
-        text += f'{type_vehicle}s count = {label.count(type_vehicle)}\n'
+        text += f'{type_vehicle}s count: {label.count(type_vehicle)}\n'
         summary_vehicle += label.count(type_vehicle)
     text += f'Summary: {summary_vehicle}'
 
     output_image = draw_bbox(frame, bbox, label, conf)
     send_image(cv2.imencode('.jpeg', frame)[1].tostring())
-    send_message(f"{text} frames_computed = {frames_computed}")
+    send_message(f"{text} frames_computed: {frames_computed}")
 
 
